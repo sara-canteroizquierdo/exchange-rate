1f4373378bf8e605629c1c31cdca7196144e4c5a
sara-canteroizquierdo <149098338+sara-canteroizquierdo@users.noreply.github.com>
2023-11-06 15:11:36 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,23 +1,3 @@
-import requests
-
-# Define the base URL and your access key
-base_url = "http://api.exchangeratesapi.io/v1/latest"
-access_key = "8d33b72224d5396cbf4aa532d86051a8"  # Replace with your actual access key
-
-# Construct the full URL
-url = f"{base_url}?access_key={access_key}"
-
-# Send a GET request to the API
-response = requests.get(url)
-
-# Check the HTTP status code
-if response.status_code == 200:
-    # Request was successful, print the JSON response
-    data = response.json()
-    print(data)
-else:
-    # Request failed, print an error message
-    print(f"Request failed with status code: {response.status_code}")
 import streamlit as st
 from streamlit.logger import get_logger
 
